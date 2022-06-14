42b3c6cd058d0856e84ff44df4885d28fc2c3415
AlexHome <foster13@yandex.ru>
2022-06-14 10:09:29 +0300

    add mask phone

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,5 +1,6 @@
 //====================popup==================
 let modal = document.querySelector('.modal');
+let body = document.querySelector('body');
 let btn = document.querySelectorAll('.popup_btn');
 let close = document.querySelector('.modal__close');
 let popup = document.querySelector('.modal__content');
@@ -7,14 +8,17 @@ let popup = document.querySelector('.modal__content');
 for (let i = 0; i < btn.length; i++) {
     btn[i].addEventListener('click', function(){
       modal.style.display = 'block';
+      body.style.overflow = 'hidden';
     });
 }
 close.onclick = function () {
   modal.style.display = 'none';
+  body.style.overflow = 'scroll';
 }
 window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
+      body.style.overflow = 'scroll';
     }
 }
 
@@ -194,7 +198,7 @@ function setTimer(selector, endtime) {
 }
 setTimer(".timer", deadline);
 
-//=====================faq================
+//=====================faq ================
 
 const questions = document.querySelectorAll(".question");
 const answers = document.querySelectorAll(".answer");
@@ -215,3 +219,48 @@ plus.forEach((item) => {
 
   });
 });
+
+
+//========================phone mask=================
+
+function maskPhone(selector, masked = '+7 (___) ___-__-__') {
+	const elems = document.querySelectorAll(selector);
+
+	function mask(event) {
+		const keyCode = event.keyCode;
+		const template = masked,
+			def = template.replace(/\D/g, ""),
+			val = this.value.replace(/\D/g, "");
+		console.log(template);
+		let i = 0,
+			newValue = template.replace(/[_\d]/g, function (a) {
+				return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+			});
+		i = newValue.indexOf("_");
+		if (i !== -1) {
+			newValue = newValue.slice(0, i);
+		}
+		let reg = template.substr(0, this.value.length).replace(/_+/g,
+			function (a) {
+				return "\\d{1," + a.length + "}";
+			}).replace(/[+()]/g, "\\$&");
+		reg = new RegExp("^" + reg + "$");
+		if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+			this.value = newValue;
+		}
+		if (event.type === "blur" && this.value.length < 5) {
+			this.value = "";
+		}
+
+	}
+
+	for (const elem of elems) {
+		elem.addEventListener("input", mask);
+		elem.addEventListener("focus", mask);
+		elem.addEventListener("blur", mask);
+	}
+	
+}
+
+maskPhone('#phone',)
+
